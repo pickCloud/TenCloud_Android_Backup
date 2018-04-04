29a18f1f91fc33c41e1d4087c32bb1ec40ae87dc
陈新海 <haigod7@gmail.com>
2018-04-04 18:39:51 +0800

    新增标签参照微信

--- app/src/main/java/com/ten/tencloud/bean/LabelBean.java
+++ app/src/main/java/com/ten/tencloud/bean/LabelBean.java
@@ -1,12 +1,20 @@
 package com.ten.tencloud.bean;
 
+import android.support.annotation.NonNull;
+
 /**
  * Create by chenxh@10.com on 2018/4/3.
  */
-public class LabelBean {
+public class LabelBean implements Comparable<LabelBean>{
 
     private String name;
-    private boolean isCheck;
+    private boolean isCheck;//历史标签选中控制
+    private boolean isSelect;//编辑标签的实际单选状态控制
+    private boolean isDelete;//编辑标签，再次点击删除
+
+    public LabelBean(String name) {
+        this.name = name;
+    }
 
     public LabelBean(String name, boolean isCheck) {
         this.name = name;
@@ -29,5 +37,24 @@ public class LabelBean {
         isCheck = check;
     }
 
+    public boolean isDelete() {
+        return isDelete;
+    }
 
+    public void setDelete(boolean delete) {
+        isDelete = delete;
+    }
+
+    public boolean isSelect() {
+        return isSelect;
+    }
+
+    public void setSelect(boolean select) {
+        isSelect = select;
+    }
+
+    @Override
+    public int compareTo(@NonNull LabelBean o) {
+        return this.getName().compareTo(o.getName());
+    }
 }
